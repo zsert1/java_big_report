f4bf0274e5d1fe6c0a382ee5fc40b1e3273e7f0b
zsert1 <zsert1@naver.com>
2024-06-19 22:40:29 +0900

    회원가입 기능API +MAPPER

--- src/main/java/com/bigboard/boardserver/service/impl/UserServiceImpl.java
+++ src/main/java/com/bigboard/boardserver/service/impl/UserServiceImpl.java
@@ -2,14 +2,27 @@ package com.bigboard.boardserver.service.impl;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bigboard.boardserver.dto.UserDTO;
 import com.bigboard.boardserver.exception.DuplicatedException;
+import com.bigboard.boardserver.mapper.UserProfileMapper;
 import com.bigboard.boardserver.service.UserService;
+import static com.bigboard.boardserver.util.SHA256Util.encryptSHA256;
+import lombok.extern.log4j.Log4j2;
 
 @Service
+@Log4j2
 public class UserServiceImpl implements UserService{
+   
+   
+    @Autowired
+    private UserProfileMapper userProfileMapper;
+
+    public UserServiceImpl(UserProfileMapper userProfileMapper) {
+        this.userProfileMapper = userProfileMapper;
+    }
 
     @Override
     public void register(UserDTO userProfile) {
@@ -18,7 +31,13 @@ public class UserServiceImpl implements UserService{
             throw new DuplicatedException("중복된 아이디 입니다.");
         }
         userProfile.setCreateTime(new Date());
-        // userProfile.setPassword()
+        userProfile.setPassword(encryptSHA256(userProfile.getPassword()));
+        int insertCount =userProfileMapper.register(userProfile);
+
+        if(insertCount !=1){
+            log.error("insert Error 회원가입 메서드 확인 필요\n" +"Params:"+ userProfile);
+        }
+         // userProfile.setPassword()
         // // TODO Auto-generated method stub
         // throw new UnsupportedOperationException("Unimplemented method 'register'");
     }
@@ -31,8 +50,8 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public boolean isDuplicated(String id) {
+        return userProfileMapper.idCheck(id)==1;
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isDuplicated'");
     }
 
     @Override
