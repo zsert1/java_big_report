73587bd736730f6f0ecade33d40af3cc50542f36
zsert1 <zsert1@naver.com>
2024-06-27 00:29:30 +0900

    에러 수정 완료

--- src/main/java/com/bigboard/boardserver/config/MysqlConfig.java
+++ src/main/java/com/bigboard/boardserver/config/MysqlConfig.java
@@ -5,20 +5,25 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @Configuration
-@MapperScan(basePackages = "com.bigboard.boardserver.mapper")
+@MapperScan(basePackages = "com.bigboard.boardserver")
 public class MysqlConfig {
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
 
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
-        final SqlSessionFactoryBean sessionFactory= new SqlSessionFactoryBean();
-        PathMatchingResourcePatternResolver resolver=new PathMatchingResourcePatternResolver();
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         sessionFactory.setMapperLocations(resolver.getResources("classpath:mappers/*.xml"));
-        Resource mybatisConfig= new PathMatchingResourcePatternResolver().getResource("classpath:mybatis-config.xml");
-        sessionFactory.setConfigLocation(mybatisConfig);
+
+        Resource myBatisConfig = new PathMatchingResourcePatternResolver().getResource("classpath:mybatis-config.xml");
+        sessionFactory.setConfigLocation(myBatisConfig);
+
         return sessionFactory.getObject();
     }
 
