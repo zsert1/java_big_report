0b60a69a5c5715c3325f0d8f016c2c78fecd2e4c
zsert1 <zsert1@naver.com>
2024-06-26 23:51:27 +0900

    에러수정 작업중

--- src/main/java/com/bigboard/boardserver/BoardserverApplication.java
+++ src/main/java/com/bigboard/boardserver/BoardserverApplication.java
@@ -1,9 +1,11 @@
 package com.bigboard.boardserver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.bigboard.boardserver.mapper")
 public class BoardserverApplication {
 
 	public static void main(String[] args) {
