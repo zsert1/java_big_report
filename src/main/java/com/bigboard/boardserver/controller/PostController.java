1d9a5c626b56b7041057527a6e56c131b1cc7dba
zsert1 <zsert1@naver.com>
2024-07-01 23:12:20 +0900

    매퍼 및 게시판 컨트롤러 작업

--- src/main/java/com/bigboard/boardserver/controller/PostController.java
+++ src/main/java/com/bigboard/boardserver/controller/PostController.java
@@ -1,12 +1,30 @@
 package com.bigboard.boardserver.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bigboard.boardserver.aop.LoginCheck;
+import com.bigboard.boardserver.dto.PostDTO;
+import com.bigboard.boardserver.dto.UserDTO;
+import com.bigboard.boardserver.dto.response.CommonResponse;
 import com.bigboard.boardserver.service.impl.PostServiceImpl;
 import com.bigboard.boardserver.service.impl.UserServiceImpl;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
+import java.util.List;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/posts")
@@ -20,5 +38,77 @@ public class PostController {
         this.postService = postService;
         this.userService = userService;
     }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @LoginCheck(type = LoginCheck.UserType.USER)
+    public ResponseEntity<CommonResponse<PostDTO>> registerPost(String accountId, @RequestBody PostDTO postDTO) {
+        postService.register(accountId, postDTO);
+        CommonResponse commonResponse = new CommonResponse<>(HttpStatus.OK, "SUCCESS", "registerPost", postDTO);
+        return ResponseEntity.ok(commonResponse);
+    }
 
+    @GetMapping("my-posts")
+    @LoginCheck(type = LoginCheck.UserType.USER)
+    public ResponseEntity<CommonResponse<List<PostDTO>>> myPostInfo(String accountId) {
+        UserDTO memberInfo = userService.getUserInfo(accountId);
+        List<PostDTO> postDTOList = postService.getMyProducts(memberInfo.getId());
+        CommonResponse commonResponse = new CommonResponse<>(HttpStatus.OK, "SUCCESS", "myPostInfo", postDTOList);
+        return ResponseEntity.ok(commonResponse);
+    }
+
+    @PatchMapping("{postId}")
+    @LoginCheck(type = LoginCheck.UserType.USER)
+    public ResponseEntity<CommonResponse<PostRequest>> updatePosts(String accountId,
+                               @PathVariable(name = "postId") int postId,
+                               @RequestBody PostRequest postRequest) {
+        UserDTO memberInfo = userService.getUserInfo(accountId);
+        PostDTO postDTO = PostDTO.builder()
+                .id(postId)
+                .name(postRequest.getName())
+                .contents(postRequest.getContents())
+                .views(postRequest.getViews())
+                .categoryId(postRequest.getCategoryId())
+                .userId(memberInfo.getId())
+                .fileId(postRequest.getFileId())
+                .updateTime(new Date())
+                .build();
+        postService.updateProducts(postDTO);
+        CommonResponse commonResponse = new CommonResponse<>(HttpStatus.OK, "SUCCESS", "updatePosts", postDTO);
+        return ResponseEntity.ok(commonResponse);
+    }
+    @DeleteMapping("{postId}")
+    @LoginCheck(type = LoginCheck.UserType.USER)
+    public ResponseEntity<CommonResponse<PostDeleteRequest>> deleteposts(String accountId,
+                               @PathVariable(name = "postId") int postId,
+                               @RequestBody PostDeleteRequest postDeleteRequest) {
+        UserDTO memberInfo = userService.getUserInfo(accountId);
+        postService.deleteProduct(memberInfo.getId(), postId);
+        CommonResponse commonResponse = new CommonResponse<>(HttpStatus.OK, "SUCCESS", "deleteposts", postDeleteRequest);
+        return ResponseEntity.ok(commonResponse);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    private static class PostResponse {
+        private List<PostDTO> postDTO;
+    }
+
+    @Setter
+    @Getter
+    private static class PostRequest {
+        private String name;
+        private String contents;
+        private int views;
+        private int categoryId;
+        private int userId;
+        private int fileId;
+        private Date updateTime;
+    }
+
+    @Setter
+    @Getter
+    private static class PostDeleteRequest {
+        private int id;
+        private int accountId;
+    }
 }
